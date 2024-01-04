3297e47f3abec7ed61417b8a9e9fe9e4a0ba2513
Bhavani R <bhavaniai01@brightskills.com>
2024-01-04 13:29:41 +0530

    even or odd file

---
+++ evennumber.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    {
+    printf("Enter a number");
+    scanf("%d",&a);
+    }
+    if(a>0)     
+    {
+        printf("even number");
+
+    }
+    else 
+       {
+        printf("odd number");
+    }
+    return 0;
+       
+ }
+    
